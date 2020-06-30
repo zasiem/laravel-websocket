4e4d23922a272127819b11efd27c9975e1459133
Erza Putra Albasori <erzaputra9110@gmail.com>
2020-07-01 00:21:24 +0700

    add: demo event channel broadcast, but cannot listen in client

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -36,8 +36,15 @@ window.Pusher = require('pusher-js');
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     wsHost: window.location.hostname,
     wsPort: 6001,
+    wssPort: 6001,
     disableStats: true,
+    enabledTransports: ["ws", "wss"],
+});
+
+window.Echo.channel('DemoChannel')
+.listen('DemoEvent', (e) => {
+    console.log(e);
+    console.log("a");
 });
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Events\DemoEvent;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    broadcast(new DemoEvent('data testing'));
     return view('welcome');
 });
 
